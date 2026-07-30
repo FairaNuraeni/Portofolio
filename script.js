7a432ac3fb646c7ed03378d9d478717e905d7680
fairanuraeni <FairaNuraeni.05>
2026-07-30 10:42:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link=>{
+    link.addEventListener("click",()=>{
+        links.forEach(item=>item.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
+
+console.log("Portfolio Ready");
